26c97997798860a04a903a52af8d9d1f310f7b0b
James Charnley <james.charnley@mds.ac.nz>
2022-06-22 10:15:05 +1200

    tileset2op

---
+++ TileSet20p.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="TileSet20p" tilewidth="16" tileheight="16" tilecount="1936" columns="44">
+ <image source="Love2D/Mossy Tileset/Mossy - TileSet20p.png" width="717" height="717"/>
+</tileset>
